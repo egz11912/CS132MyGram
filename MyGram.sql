bb54a8b0371e0b3641fb2f303d33db5dd2d33b0a
Eric Zuniga <egz11912@gmail.com>
2021-04-16 17:12:19 -0600

    Assignment Module 4

---
+++ MyGram.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE MYGRAM;
+
+USE MYGRAM;
+
+CREATE TABLE IMAGE(
+ImageId              int,
+imageName            varchar(500),
+ImagePath            varchar(500),
+ImageDescription     varchar(500),
+ImageAltText         varchar(500)
+);
+
+
